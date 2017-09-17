5fbb4455d1cc68528b8fc9bd48203645b0a56fc8
Aliaksandr Shumeika <unumtresocto@gmail.com>
2017-09-17 23:06:25 +0300

    task-2: fixed formatting

--- src/presentational/movie-preview.jsx
+++ src/presentational/movie-preview.jsx
@@ -5,8 +5,8 @@ const MoviePreview = ({ coverLink, title, year, genre }) => (
         <img className="movie-preview__cover" src={ coverLink } alt=""/>
         <div className="movie-preview__footer">
             <h3 className="moview-preview__name">{ title }</h3>
-        <label className="movie-preview__year">{ year }</label>
-        <p className="moview-preview__genre">{ genre }</p>
+            <label className="movie-preview__year">{ year }</label>
+            <p className="moview-preview__genre">{ genre }</p>
         </div>
     </div>
 );
